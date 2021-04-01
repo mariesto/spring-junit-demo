8e841aafa8f44e1381b9d6b66153d143d53466b6
Amendo Mariesto Sitinjak <amendo.sitinjak@mhealth.tech>
2021-04-01 17:17:24 +0700

    Initial commit

---
+++ src/main/java/com/mariesto/bookstoreadapp/BookstoreadAppApplication.java
@@ -0,0 +1,12 @@
+package com.mariesto.bookstoreadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreadAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreadAppApplication.class, args);
+    }
+
+}
